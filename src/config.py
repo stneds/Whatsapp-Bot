26d8d99865bb32af48c513357fb96f02f504e648
Antonio Santana <antoniosantana4263@gmail.com>
2026-01-19 12:49:28 -0300

    Fix envio de mensagem no WhatsApp Web (remove headless e ajustes Selenium)

--- src/config.py
+++ src/config.py
@@ -1,8 +1,18 @@
-WHATSAPP_NUMBER = "5586981271683"  # coloque o número com DDI
-MESSAGE = "Bom dia namoradinha fofinha lindinha! 🌞❤️"  # coloque a mensagem que deseja enviar
+import os
+
+WHATSAPP_NUMBER = os.getenv("WHATSAPP_NUMBER")
+MESSAGE = ( "Bom dia namorada")
+
 SEND_HOUR = 6
-SEND_MINUTE = 45
+SEND_MINUTE = 58
 
+CHROME_PROFILE_PATH = os.getenv(
+    "CHROME_PROFILE_PATH",
+    "/opt/whatsapp-bot/chrome/profile"
+)
 
-
-CHROME_PROFILE_PATH = "/opt/whatsapp-bot/chrome/profile"
+print("DEBUG CONFIG:")
+print("WHATSAPP_NUMBER =", WHATSAPP_NUMBER)
+print("CHROME_PROFILE_PATH =", CHROME_PROFILE_PATH)
+print("SEND_HOUR =", SEND_HOUR)
+print("SEND_MINUTE =", SEND_MINUTE)
